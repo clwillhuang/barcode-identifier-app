3a4e779eaebffcbdf4585cb1c204b0039dba3736
What-Is-A-Username <49571870+What-Is-A-Username@users.noreply.github.com>
2022-12-22 16:16:02 -0500

    Remove file

--- src/url.js
+++
@@ -1,5 +0,0 @@
-// If API is on local server, change to localhost:
-// export const urlRoot = 'http://127.0.0.1:8000'
-
-// If API is on remote server (e.g. AWS), change to IP or domain address:
-export const urlRoot = 'https://3.17.163.210'
